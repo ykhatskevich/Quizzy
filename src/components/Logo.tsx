dc69a7dd32ccff73510deb02cf2bc04f43adecea
Yan Khatskevich <hackiewiczjan@gmail.com>
2023-09-27 13:17:24 +0200

    Styled Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,10 +3,10 @@ export default function Logo () {
         <>
         <div className="flex-col items-center justify-center text-center">
         <div className="flex flex-row items-center justify-center gap-2">
-            <h1 className="text-6xl font-bold text-teal-300" style={{ fontFamily: 'Fuggles, cursive' }}>Quizzy</h1>
+            <h1 className="text-8xl font-bold text-amber-500" style={{ fontFamily: 'Fuggles, cursive' }}>Quizzy</h1>
             <img src='/public/question-mark.png' alt='question mark' className='w-10 h-10' />
         </div>
-        <h3 className="text-teal-500 mt-2" style={{fontFamily:'IBM PLEX MONO, monospace'}}>Unleash Your Knowledge!</h3>
+        <h3 className="text-indigo-500 text-xl mt-2" style={{fontFamily:'IBM PLEX MONO, monospace'}}>Unleash Your Knowledge!</h3>
         </div>
         </>
     )
